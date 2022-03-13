04a7d118e833204d18d274ba821aee1664e03223
Noodles <20896419+callmenoodles@users.noreply.github.com>
2022-03-13 06:23:12 +0100

    Spelling / Grammar
    Don"t > Don't

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -10,7 +10,7 @@ export default function About() {
           <div className="about-underline"></div>
         </div>
         <p>
-          Hello, I go by Noodles and I am a developer of websites, apps, games, and other software. Besides, I am a translator and privacy advocate (hence I don"t use my real name).<br/><br/>
+          Hello, I go by Noodles and I am a developer of websites, apps, games, and other software. Besides, I am a translator and privacy advocate (hence I don't use my real name).<br/><br/>
           This website is mainly for showing my portfolio but I also want to use my skills to help businesses and people like you to create or update your website.<br/><br/>
           Interested? Have a look at my portfolio below and get in touch with me.
         </p>
