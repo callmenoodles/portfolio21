d3d6acb692ba5624197900f74332d644434a9820
Noodles <20896419+callmenoodles@users.noreply.github.com>
2022-03-13 06:24:25 +0100

    Update email
    Use alias

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -8,7 +8,7 @@ import iconLI from "../../res/social/linkedin.jxl"
 export default function Footer() {
   return (
     <footer id="contact">
-      <a href="mailto:anxlus@tuta.io"><button>Get In Touch</button></a>
+      <a href="mailto:ca4ffa9b-3c43-442a-b0a6-c9ed594978fd@aleeas.com"><button>Get In Touch</button></a>
       <ul>
         <Link to="section-header" smooth={"easeOutQuint"}>
           <li style={{"marginRight": "25px"}}>Home</li>
